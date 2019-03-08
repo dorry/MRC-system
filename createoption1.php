92d7859dc4a90d6413bedcea792666b22ada8704
shehababoelrejal <shobymoh@gmail.com>
2019-03-08 19:22:15 +0200

    heta oo

--- createoption1.php
+++ createoption1.php
@@ -1,5 +1,5 @@
 <?php
-
+include "useroptions.php";
 session_start();
 if(!empty($_SESSION))
 {
@@ -9,19 +9,11 @@ else
 {
   header("Location:index.php");
 }
-$servername = "localhost";
-$username = "id8878100_root";
-$pasword = "fz@ayV3V#@2W!Zd^1qwN";
-$dbname = "id8878100_mrc";
-// create connection
-$conn = new mysqli ($servername, $username, $pasword, $dbname);
-
-if(isset($_POST['option_submit']))
-{ 
 	$R = $_POST['name'];
 	$D = $_POST['datatype'];
-	$sql = "Insert INTO useroptions (name,type) values('".$R."','".$D."')";
-  mysqli_query($conn,$sql);
-	header("Location:UTD.php");
-	echo "<script>alert('A New Option  has been created')</script>";
+	$useroptions = new useroptions();
+	$useroptions->name = $R;
+	$useroptions->type = $D;
+	$useroptions->adduseroptions($useroptions);
+
 }
--- useroptions.php
+++ useroptions.php
@@ -13,7 +13,7 @@ static function adduseroptions ($obj)
 {
     $DB=new database();
     $conn=$DB->DBC();
-	$sql = "Insert INTO useroptions (name,type) values('".$R."','".$D."')";
+	$sql = "Insert INTO useroptions (name,type) values('$obj->name','$obj->type')";
     mysqli_query($conn,$sql);
 	header("Location:UTD.php");
 	echo "<script>alert('A New Option  has been created')</script>";
