a64d8d70d66d9235cbf0dedc2d75da42ed797d6c
reda83 <34404006+reda83@users.noreply.github.com>
2019-03-15 23:08:17 +0200

    Class Diagram Changes

---
+++ Cart.php
@@ -0,0 +1,28 @@
+<?php
+require_once"mydatabaseconnection.php";
+
+class Cart
+{
+public $reservedRadiologies=array();
+$radiology=new radiology();
+
+
+public static function getTotalPrice(){
+
+}
+public static function updateCart(){
+
+}
+public static function createCart(){
+
+}
+public static function deleteCart(){
+
+}
+
+
+
+
+
+
+?>
---
+++ ExtraInfo.php
@@ -0,0 +1,25 @@
+<?php 
+require_once"mydatabaseconnection.php";
+//comment
+class ExtraInfo
+{
+public $WorkingDaysHours=array();
+
+
+public static function setWorkingInfo($WorkingDaysHours){
+
+}
+public static function getWorkingInfo($WorkingDaysHours){
+
+}
+public static function showPatients(){
+
+}
+
+
+
+
+
+
+
+?>
