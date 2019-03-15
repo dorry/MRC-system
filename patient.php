eb8ff54bdde7ab4dce66d29489185db25a567ba0
Sherif Abd El Khalek <32877807+SherifElNayad@users.noreply.github.com>
2019-03-16 00:19:29 +0200

    Update patient.php

--- patient.php
+++ patient.php
@@ -3,7 +3,7 @@ include"user.php";
 include"IReserve.php";
 include"IRegister.php";
 require_once"mydatabaseconnection.php";
-class patient extends user implements IReserve,IRegister{
+class patient extends user implements Iloginn, IReserve,IRegister{
 
 public $cart;
 
