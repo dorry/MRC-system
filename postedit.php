97dcba02e3acbed9ac286679aaa3e03bd48ecf97
Sherif Abd El Khalek <32877807+SherifElNayad@users.noreply.github.com>
2019-03-16 22:12:27 +0200

    Edit EAV
    zay ma title by2ol

--- postedit.php
+++ postedit.php
@@ -1,5 +1,7 @@
 <?php
-//Not Edited to Object Oriented
+require_once"mydatabaseconnection.php";
+$DB=new database();
+$conn=$DB->DBC();
 
 session_start();
 if(!empty($_SESSION))
@@ -14,11 +16,56 @@ else
 $i = 0;
 
 if(isset($_POST['eutd_submit'])){
-    $R = $_POST['roleid'];
- echo "<script>alert('$R')</script>";
+  $R = $_POST['roleid'];
+  $U =  $_POST['user'];
 
-    $U = $_POST['users'];
-    echo "<script>alert('$U')</script>";
-
+  $sql5 = "SELECT  * FROM `usertypeoptions` where userTypeId=$R" ;
+	$result5 = mysqli_query($conn, $sql5);
+ 		if(mysqli_num_rows($result5) > 0){
+			$oname = array();
+			$usertypeoptions = array();
+     		while($row = mysqli_fetch_array($result5))
+    	{
+    		$OID = $row['optionsId'];
+			$sql6 = "SELECT  * FROM `useroptions` where id= '$OID';";
+			$result6 = mysqli_query($conn, $sql6);
+				 if(mysqli_num_rows($result6) > 0){
+		   	     while($rowUsOp = mysqli_fetch_array($result6))
+		 		  {
+					
+					   array_push($oname, $rowUsOp['name']);
+                      
+                   }
+                }
+            }
+        }
+	$get_uto_id = "select id from usertypeoptions where userTypeId =$R" ;
+	//echo $get_uto_id;
+  $result_uto = mysqli_query($conn, $get_uto_id);
+  $sql10 = "SELECT userTyOpId from useropvalue where userId = $U";
+  $result10 =mysqli_query($conn,$sql10);
+  // secho $sql10;
+  if(mysqli_num_rows($result10) > 0){
+    while($row55= mysqli_fetch_array($result10))
+ {
+    
+   array_push($usertypeoptions, $row55['userTyOpId']);
+            
+         }
+      }
+ 		if(mysqli_num_rows($result_uto) > 0){
+			
+     		while($row = mysqli_fetch_array($result_uto))
+    	{
+			 $insert_values = "UPDATE useropvalue
+        SET value='".$_POST[$oname[$i]]."' 
+        WHERE userTyOpId = ".$usertypeoptions[$i]." " ;
+            echo $insert_values;
+             mysqli_query($conn , $insert_values);
+			       $i++;
+    }
+  
+     }
+     
 			}
  ?>
--- postmodify.php
+++ postmodify.php
@@ -1,5 +1,7 @@
 <?php
-//Not Edited to Object Oriented
+require_once"mydatabaseconnection.php";
+$DB=new database();
+$conn=$DB->DBC();
 
 session_start();
 if(!empty($_SESSION))
@@ -15,7 +17,7 @@ $i = 0;
 
 if(isset($_POST['utd_submit'])){
 	$R = $_POST['roleid'];
-	$sql5 = "SELECT  * FROM `usertypeoptions` where userTypeId= (select id from usertype where type = $R)" ;
+	$sql5 = "SELECT  * FROM `usertypeoptions` where userTypeId = $R" ;
 	$result5 = mysqli_query($conn, $sql5);
  		if(mysqli_num_rows($result5) > 0){
 			$oname = array();
@@ -24,17 +26,16 @@ if(isset($_POST['utd_submit'])){
     		$OID = $row['optionsId'];
 			$sql6 = "SELECT  * FROM `useroptions` where id= '$OID';";
 			$result6 = mysqli_query($conn, $sql6);
-				 if(mysqli_num_rows($result6) > 0){
+			if(mysqli_num_rows($result6) > 0){
 		   	     while($rowUsOp = mysqli_fetch_array($result6))
 		 		  {
-					
 					   array_push($oname, $rowUsOp['name']);
                       
                    }
                 }
             }
         }
-	$get_uto_id = "select id from usertypeoptions where userTypeId =(select id from usertype where type = $R)" ;
+	$get_uto_id = "select id from usertypeoptions where userTypeId =$R" ;
 	echo $get_uto_id;
     $result_uto = mysqli_query($conn, $get_uto_id);
     
@@ -44,12 +45,17 @@ if(isset($_POST['utd_submit'])){
      		while($row = mysqli_fetch_array($result_uto))
     	{
 
-			 $insert_values = "insert into useropvalue (userTyOpId , userId , value) values (" . $row['id'] .",". $_POST['user']. ",'" . $_POST[$oname[$i]]."' )" ;
+			 $insert_values = 
+			 "insert into useropvalue (userTyOpId , userId , value) 
+			 values (" . $row['id'] .",". $_POST['user']. ",'" . $_POST[$oname[$i]]."' )" ;
             echo $insert_values;
              mysqli_query($conn , $insert_values);
 			echo $oname[$i];
 			$i++;
+			
 		}
+		header("Location:UTD.php");
+
 		 }
 			}
  ?>
