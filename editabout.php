31018addcef6090128afe0c6dd1d83e2aa144342
reda83 <34404006+reda83@users.noreply.github.com>
2019-03-17 18:35:56 +0200

    ckEDITOOOOR

--- editabout.php
+++ editabout.php
@@ -1,4 +1,12 @@
 <?php
+echo'<html>
+<head>
+        <meta charset="utf-8">
+        <title>CKEditor</title>
+        <script src="https://cdn.ckeditor.com/4.11.3/standard/ckeditor.js"></script>
+</head>
+<body>
+';
 require_once"mydatabaseconnection.php";
 $DB=new database();
 $conn=$DB->DBC();
@@ -8,7 +16,7 @@ $result = mysqli_query($conn, $query);
 if(mysqli_num_rows($result) > 0){
     if($row = mysqli_fetch_array($result))
    {    echo "<form method = 'Post'>";
-        echo"<pre> <textarea name = 'code' style ='width:100%;height:100%'  >";
+        echo"<pre> <textarea name = 'editor1' style ='width:100%;height:100%'  >";
         echo  htmlspecialchars($row['content']);
       echo "</textarea>";
        echo" </pre> ";
@@ -17,9 +25,16 @@ if(mysqli_num_rows($result) > 0){
     }}
     
     if(isset($_POST['submit'])){
-    $code = $_POST['code'];
+    $code = $_POST['editor1'];
     $code = mysqli_real_escape_string($conn,$code);
     $applyChangesQuery = "update static set content='$code' where id =1";
     mysqli_query($conn,$applyChangesQuery);
     }
+echo'<script>
+CKEDITOR.replace( "editor1" );
+</script>
+</body>
+</html>';
 ?>
+
+                
