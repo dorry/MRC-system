0dd96e276b75df2df21cf6cb0889ec665640b468
reda83 <34404006+reda83@users.noreply.github.com>
2019-04-25 20:59:39 +0200

    +1

--- about.php
+++ about.php
@@ -1,16 +1,12 @@
 
 <?php 
 require_once"mydatabaseconnection.php";
-$DB=new database();
-$conn=$DB->DBC();
-
-$query = "SELECT  content  FROM static WHERE id = 1";
-$result = mysqli_query($conn, $query);
-if(mysqli_num_rows($result) > 0){
+$DB=database::getinstance();
+$result=$DB->query("static","id = 1");
     if($row = mysqli_fetch_array($result))
    {
 
        echo $row['content'];
-    }}
+    }
     
 ?>
