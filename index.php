96421bd653fa21f3cb776de33b165591f4ef6b2e
reda83 <34404006+reda83@users.noreply.github.com>
2019-05-19 05:41:42 +0200

    bagarb a3ml translate

--- index.php
+++ index.php
@@ -383,7 +383,15 @@ session_start();
         </div>
     </section>
     <!-- News Area Starts -->
-    
+    <div id="google_translate_element"></div>
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <!-- Footer Area Starts -->
     <?php include("footer.php"); ?>
     <!-- Footer Area End -->
